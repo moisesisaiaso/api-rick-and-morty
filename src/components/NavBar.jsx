8e6ab0e3fae92d4902494dac4122d94fbf7e7df8
Moisés Isaías Ortíz Gracia <moisesisaias23mar@gmail.com>
2024-02-10 03:10:08 -0500

    api-rick-and-morty-5

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,40 +9,13 @@ import headerStyles from "../assets/styles/headerStyles.module.css";
 /* hooks */
 import { useState } from "react";
 export const NavBar = () => {
-    const [navMenu, setNavMenu] = useState();
-    const [navList, setNavList] = useState();
+    const [navList, setNavList] = useState(false);
     const onHandleOpenMenu = () => {
-        const navListStyle = {
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-        };
-
-        setNavList(navListStyle);
-
-        const navMenuStyle = {
-            display: "none",
-        };
-
-        setNavMenu(navMenuStyle);
+        setNavList(true);
     };
 
     const onHandleExitMenu = () => {
-        const anchoPantalla = document.documentElement.clientWidth;
-        console.log("pantalla", anchoPantalla);
-        if (anchoPantalla < "600") {
-            const navListStyle = {
-                display: "none",
-            };
-            setNavList(navListStyle);
-
-            const navMenuStyle = {
-                display: "block",
-            };
-
-            setNavMenu(navMenuStyle);
-        }
+        setNavList(false);
     };
     return (
         <>
@@ -52,12 +25,13 @@ export const NavBar = () => {
                 </Link>
                 {/* Link es un componente que me permite renderizar solo las partes que cambian sin recargar el sitio, es decir utiliza el DOM virtual */}
 
-                <ul className={headerStyles.nav__list} style={navList} onClick={onHandleExitMenu}>
+                <ul
+                    className={headerStyles.nav__list + " " + `${navList && headerStyles.enable}`}
+                    onClick={onHandleExitMenu}
+                >
                     <NavLink
                         className={({ isActive }) =>
-                            isActive
-                                ? ` ${headerStyles.route__active} `
-                                : ` ${headerStyles.route__inactive} `
+                            headerStyles.route + " " + `${isActive && headerStyles.active} `
                         }
                         to="/"
                     >
@@ -66,9 +40,7 @@ export const NavBar = () => {
 
                     <NavLink
                         className={({ isActive }) =>
-                            isActive
-                                ? ` ${headerStyles.route__active} `
-                                : ` ${headerStyles.route__inactive} `
+                            headerStyles.route + " " + `${isActive && headerStyles.active} `
                         }
                         to="location"
                     >
@@ -77,9 +49,7 @@ export const NavBar = () => {
 
                     <NavLink
                         className={({ isActive }) =>
-                            isActive
-                                ? ` ${headerStyles.route__active} `
-                                : ` ${headerStyles.route__inactive} `
+                            headerStyles.route + " " + `${isActive && headerStyles.active} `
                         }
                         to="episode"
                     >
@@ -87,7 +57,10 @@ export const NavBar = () => {
                     </NavLink>
                 </ul>
 
-                <div className={headerStyles.nav__menu} style={navMenu} onClick={onHandleOpenMenu}>
+                <div
+                    className={headerStyles.nav__menu + " " + `${navList && headerStyles.disable}`}
+                    onClick={onHandleOpenMenu}
+                >
                     <img src={menuHamburgesa} alt="menu" />
                 </div>
             </div>
